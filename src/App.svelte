ee5c88cad6b9865be8ddb148463ffbd545eaff5c
Flore Perche <flore.perche@gmail.com>
2024-07-12 19:15:03 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { scaleLinear } from "d3";
+  let width;
+</script>
+
+<div bind:clientWidth={width} class="container">
+  <div
+    style="width: {width / 4}px; background-color: #5C5C6C; "
+    class="scale"
+  ></div>
+  <div
+    style="width: {width / 2}px; background-color: #5C5C5C;"
+    class="viz"
+  ></div>
+  <div
+    style="width: {width / 4}px; background-color: #7EA755; "
+    class="background"
+  ></div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+  }
+
+  .container div {
+    height: 100vh;
+  }
+</style>
