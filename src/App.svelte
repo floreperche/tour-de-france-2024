46f3d0c9b49fd173cd038280d6e132b12aa5d8e2
Flore Perche <flore.perche@gmail.com>
2024-07-15 13:26:05 -0300

    yScale, database v2

--- src/App.svelte
+++ src/App.svelte
@@ -2,110 +2,130 @@
   import { scaleLinear } from "d3";
   import Scrolly from "./helpers/Scrolly.svelte";
   import initialStageData from "./assets/stage_results.json";
+  import riders from "./assets/riders.json";
+  import StepCount from "./components/StepCount.svelte";
+
+  import YScale from "./components/YScale.svelte";
 
   let width;
   let height;
 
-  // console.log(initialStageData[0].riders);
+  const stageCount = Array(3);
 
-  let randeredStageData = initialStageData[0].riders;
-
-  // let randeredData = data
+  let renderedData = riders;
+  let displayedResults;
 
   $: xScale = scaleLinear()
     .domain([0, 4])
-    .range([40, width - 40]);
+    .range([40, width / 2 - 40]);
 
   $: yScale = scaleLinear()
-    .domain([0, 100])
+    .domain([0, 130])
     .range([height - 150, 10]);
 
   let currentStep;
 
   $: {
     if (currentStep === undefined) {
-      randeredStageData = initialStageData[0].riders;
+      renderedData = riders;
     } else {
-      initialStageData.map((stage) => {
-        if (stage.id === currentStep + 1) {
-          randeredStageData = stage.riders;
+      initialStageData.map((e) => {
+        if (e.id === currentStep + 1) {
+          displayedResults = e.riders;
         }
+      });
+      renderedData = riders.map((obj, i) => {
+        return {
+          ...obj,
+          rank: displayedResults[i].rank,
+          time: displayedResults[i].time,
+        };
       });
     }
   }
+
+  // $: console.log(renderedData);
 </script>
 
 <div class="container">
   <div class="header"></div>
   <section>
-    <div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
-      <div class="position: relative">
-        <svg width="50vw" height="100vh">
-          {#each randeredStageData as rider}
+    <div class="chart-container">
+      <div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
+        <YScale {currentStep} {yScale} />
+        <svg width="50vw" height="100vh" class="viz">
+          {#each renderedData as rider}
             <circle
               cx={xScale(rider.rank)}
               cy={yScale(rider.time)}
-              r="20"
-              fill="white"
+              r="10"
+              fill={rider.color}
             ></circle>
+            <text
+              dominant-baseline="middle"
+              fill="white"
+              x="20"
+              y="0"
+              transform="translate({xScale(rider.rank)},{yScale(
+                rider.time
+              )}) rotate(-90)">{rider.name}</text
+            >
           {/each}
         </svg>
-        {#if currentStep >= 0}
-          <div class="stage">
-            Etape {currentStep + 1}
-          </div>
-        {/if}
       </div>
+      <StepCount {currentStep} {stageCount} {width} />
     </div>
 
     <Scrolly bind:value={currentStep}>
-      {#each [1, 2, 3] as text, i}
-        <div class="step step{text}" class:active={currentStep === i}>
+      {#each stageCount as stage, i}
+        <div class="step step{i + 1}" class:active={currentStep === i}>
           <div class="step-content">
-            Etape {text}
+            Etape {i + 1}
           </div>
         </div>
       {/each}
     </Scrolly>
   </section>
-  <div>END</div>
+  <!-- <div>END</div> -->
 </div>
 
 <style>
   .header {
     height: 100vh;
     width: 100vw;
-    background-color: #5c5c5c;
+    background-color: #615968;
+  }
+
+  .chart-container {
+    position: sticky;
+    width: 100vw;
+    height: 100vh;
+    top: 0%;
   }
 
   .chart {
-    background: lightblue;
-    width: 50vw;
-    height: 100vh;
-    position: sticky;
-    top: 0%;
-    margin: auto;
+    width: 100vw;
+    position: relative;
+    display: flex;
   }
 
-  svg {
-    background-color: #5c5c5c;
-  }
-
-  .stage {
-    position: absolute;
-    bottom: 50px;
-    left: 10px;
+  .viz {
+    background-color: #615968;
   }
 
   circle {
     transition:
-      cx 2000ms ease,
-      cy 2000ms ease;
+      cx 500ms ease,
+      cy 1500ms ease;
+  }
+
+  .viz text {
+    transition: transform 1500ms ease;
   }
 
   /* Scrollytelling CSS */
   .step {
-    height: 120vh;
+    height: 150vh;
     width: 25vw;
     display: flex;
     place-items: center;
---
+++ src/components/YScale.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let currentStep;
+  export let yScale;
+
+  let yTicks = [0, 60, 120];
+</script>
+
+<svg width="25vw" height="100vh" class="scale">
+  {#if currentStep >= 0}
+    {#each yTicks as tick}
+      {#if tick === 0}
+        <text
+          dominant-baseline="middle"
+          fill="white"
+          x="10"
+          y={yScale(tick) - 8}
+          >temps du
+        </text>
+        <text
+          dominant-baseline="middle"
+          fill="white"
+          x="10"
+          y={yScale(tick) + 8}>leader</text
+        >
+      {:else}
+        <text
+          dominant-baseline="middle"
+          fill="white"
+          x="10"
+          y={yScale(tick) - 8}
+          >{tick / 60} min
+        </text>
+        <text
+          dominant-baseline="middle"
+          fill="white"
+          x="10"
+          y={yScale(tick) + 8}>de retard</text
+        >{/if}
+    {/each}
+  {/if}
+</svg>
+
+<style>
+  .scale {
+    background-color: #615968;
+  }
+</style>
