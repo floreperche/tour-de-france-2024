2ba69ef2ef45e40903d6abbb1b9a355b95360ba5
Flore Perche <flore.perche@gmail.com>
2024-07-18 16:23:52 -0300

    working header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,26 +1,43 @@
 <div class="header">
   <div class="title-container">
     <div class="title">
-      <div><p>TOUR</p></div>
-      <div><p>DE FRANCE</p></div>
-      <div><p style="text-align: end">2024</p></div>
+      <div><p>LE TOUR DE</p></div>
+      <div><p>FRANCE '24</p></div>
+      <div>
+        <svg width="203" height="6"
+          ><path
+            d="M168.594 1.34775L182.129 1.69717L193.764 1.797H196.851C200.017 2.5624 201.917 3.1614 202.55 3.59401C203.5 3.99334 202.946 4.40932 200.888 4.84193L198.751 5.39102C197.959 5.69052 196.693 5.84027 194.951 5.84027C194.16 5.97338 193.606 6.02329 193.289 5.99002C192.181 5.82363 183.158 5.49085 166.219 4.99168C166.219 4.99168 163.053 4.94176 156.721 4.84193C155.455 4.84193 151.576 4.77537 145.086 4.64226C138.754 4.54243 131.551 4.49251 123.477 4.49251L118.491 4.4426L112.079 4.19301C110.021 4.19301 108.834 4.20965 108.517 4.24293H104.718C101.235 4.17637 99.2567 4.12646 98.7818 4.09318H89.0461C81.7641 4.15973 75.8277 4.19301 71.2368 4.19301L56.5146 4.24293H45.5916L41.0799 4.34276H37.5181C34.6686 4.40932 32.4523 4.4426 30.8693 4.4426L19.2339 4.69218L12.8226 4.89185C10.4481 4.99168 8.0735 4.9584 5.69894 4.79201C3.16608 4.6589 1.50389 4.47587 0.712368 4.24293C-0.237456 4.00998 -0.237456 3.61065 0.712368 3.04492C1.50389 2.4792 2.45371 2.12978 3.56184 1.99667C4.51167 1.86356 4.90744 1.74709 4.74913 1.64725C6.01556 1.41431 7.5986 1.23128 9.49825 1.09817C10.1315 0.831947 11.2396 0.715474 12.8226 0.748752C14.4057 0.78203 15.8304 0.715474 17.0968 0.549085C18.205 0.382695 19.2339 0.299501 20.1838 0.299501C22.0834 0.299501 23.8248 0.266223 25.4078 0.199667L27.3074 0.249584C27.7824 0.14975 28.4947 0.0998334 29.4446 0.0998334L31.8191 0.0499169H35.1435C36.4099 0.0499169 37.2806 0.0332779 37.7555 0C38.2304 0 39.022 0.0332778 40.1301 0.0998334C40.2884 0.0998334 56.752 0.133111 89.521 0.199667L95.2199 0.249584H101.631C105.431 0.282862 110.1 0.299501 115.641 0.299501C127.989 0.499168 136.142 0.599002 140.099 0.599002C146.115 0.599002 155.613 0.848586 168.594 1.34775Z"
+            fill="#ECB70C"
+          />
+        </svg>
+      </div>
+      <div><p class="subtitle">A scrolly visualisation</p></div>
+    </div>
+    <div style="width: 100vw; height:100px;   ">
+      <img
+        style=" max-width: 100%;
+  height: auto; margin-top: 0px"
+        src="src\assets\visuals\header-landscape.svg"
+        alt=""
+      />
     </div>
   </div>
   <div class="description">
-    Relive the battle for the Maillot Jaune and how the gap evolved between the
-    three best classified riders throught the stages.
-  </div>
-  <div class="scroll-indicator">
-    <p>SCROLL TO START THE RACE</p>
-    <div>
-      <svg width="27" height="29" viewBox="0 0 27 29"
-        ><path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M26.5521 3.17949C27.1458 2.59078 27.1499 1.63222 26.5612 1.03848C25.9725 0.444735 25.0139 0.440655 24.4202 1.02936L13.6695 11.6889L3.01005 0.938223C2.42134 0.344482 1.46278 0.340402 0.86904 0.929109C0.275299 1.51782 0.271219 2.47638 0.859926 3.07012L12.5854 14.8958L13.6513 15.9709L14.7264 14.9049L26.5521 3.17949ZM26.131 15.2844C26.7248 14.6957 26.7288 13.7371 26.1401 13.1434C25.5514 12.5496 24.5929 12.5455 23.9991 13.1343L13.2485 23.7937L2.589 13.0431C2.00029 12.4494 1.04173 12.4453 0.447989 13.034C-0.145752 13.6227 -0.149832 14.5813 0.438875 15.175L12.1643 27.0007L13.2303 28.0758L14.3053 27.0098L26.131 15.2844Z"
-          fill="#F5F5F5"
-        />
-      </svg>
+    <p>
+      Relive the battle for the Yellow jersey and how the gap evolved between
+      the three best classified riders throught the stages.
+    </p>
+
+    <div class="scroll-indicator">
+      <p>SCROLL TO START THE RACE</p>
+      <div>
+        <svg width="27" height="29" viewBox="0 0 27 29"
+          ><path
+            d="M26.5521 3.17949C27.1458 2.59078 27.1499 1.63222 26.5612 1.03848C25.9725 0.444735 25.0139 0.440655 24.4202 1.02936L13.6695 11.6889L3.01005 0.938223C2.42134 0.344482 1.46278 0.340402 0.86904 0.929109C0.275299 1.51782 0.271219 2.47638 0.859926 3.07012L12.5854 14.8958L13.6513 15.9709L14.7264 14.9049L26.5521 3.17949ZM26.131 15.2844C26.7248 14.6957 26.7288 13.7371 26.1401 13.1434C25.5514 12.5496 24.5929 12.5455 23.9991 13.1343L13.2485 23.7937L2.589 13.0431C2.00029 12.4494 1.04173 12.4453 0.447989 13.034C-0.145752 13.6227 -0.149832 14.5813 0.438875 15.175L12.1643 27.0007L13.2303 28.0758L14.3053 27.0098L26.131 15.2844Z"
+            fill="#F5F5F5"
+          />
+        </svg>
+      </div>
     </div>
   </div>
 </div>
@@ -37,36 +54,53 @@
 
   .title-container {
     width: 100vw;
-    height: 40vh;
-    background-color: #d5ebef;
+    height: 50vh;
+    background: linear-gradient(#cce0e3 90%, #615968);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
 
   .header .title {
-    padding-top: 6vh;
+    padding-top: 4vh;
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: 4px;
     margin: 0 auto;
   }
 
-  .header .title div {
-    background-color: #ecb70c;
-  }
-
   .header .title p {
-    background-color: #ecb70c;
-    width: 200px;
     padding: 2px 6px;
     margin: 0 auto;
     font-family: "Caveat Brush", cursive;
-    font-size: 36px;
+    font-size: 50px;
     letter-spacing: 3.5px;
+    text-align: justify;
+    color: #50495f;
+  }
+
+  .header .title .subtitle {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    letter-spacing: 0px;
+    text-transform: uppercase;
+    padding-top: 8px;
   }
 
   .description {
-    height: 30vh;
+    height: 40vh;
     padding: 2em;
     color: white;
+    font-size: 18px;
+    font-weight: 300;
+    letter-spacing: 0.5px;
+    line-height: 1.4;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    gap: 20px;
   }
 
   .scroll-indicator {
