459a61aa9482bf0bf72b218c9cf0c50279170ba8
Flore Perche <flore.perche@gmail.com>
2024-07-19 14:12:24 -0300

    ready to deploy 0.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tour-de-france-2024/",
   plugins: [svelte()],
-})
+});
